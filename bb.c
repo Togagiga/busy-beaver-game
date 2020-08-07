1ea81a03be57e334705035eb8c4b8c7aad39ae83
Jonas Dieker <jonasdieker05@gmail.com>
2020-08-07 21:51:34 +0200

    edit of c lang code

--- bb.c
+++ bb.c
@@ -1,7 +1,8 @@
+// C language version of python code space holder
+
 #include <stdio.h>
 int main() {
 // printf() displays the string inside quotation
-printf("Hello, Cunt!\n");
+printf("Hello, world!\n");
   return 0;
 }
-
